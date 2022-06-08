39b74a47c2ba1ca08ff961c3652e19232dbf005f
Marq_Jack <jack.huntington00@gmail.com>
2022-06-08 14:01:29 -0600

    commit

---
+++ stedi-react-native-main/Login.js
@@ -0,0 +1,36 @@
+import {useState} from "react";
+import { SafeAreaView, StyleSheet, TextInput } from "react-native";
+
+const Login = () => {
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [oneTimePassword, setOneTimePassword] = useState(null);
+
+  return (
+    <SafeAreaView>
+      <TextInput
+        style={styles.input}
+        onChangeText={setPhoneNumber}
+        value={phoneNumber}
+        placeholder="222-222-2222"
+      />
+      <TextInput
+        style={styles.input}
+        onChangeText={setOneTimePassword}
+        value={number}
+        placeholder="1234"
+        keyboardType="numeric"
+      />
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+});
+
+export default UselessTextInput;
