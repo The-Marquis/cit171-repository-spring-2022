00215943d6a84aeac57aa9f7d1fdd9dfc9d82f0b
Marq_Jack <jack.huntington00@gmail.com>
2022-06-28 14:13:25 -0600

    commit

--- stedi-react-native-main/Login.js
+++ stedi-react-native-main/Login.js
@@ -1,5 +1,25 @@
 import {useState} from "react";
+import { TouchableOpacity } from "react-native";
 import { SafeAreaView, StyleSheet, TextInput } from "react-native";
+
+const sendText= async (phoneNumber)=>{
+  console.log("PhoneNumber: ",phoneNumber);
+  await fetch('http://dev.stedi.me/twofactorlogin/'+phoneNumber),{
+    method: 'POST',
+    headers:{
+      'content-type':'application.text'
+    }
+  }
+}
+
+const getToken = async ({phoneNumber, oneTimePassword}) =>{
+  await fetch('http://dev.stedi.me/twofactorlogin/'+phoneNumber,{
+    method: 'POST',
+    body:JSON.stringify({oneTimePassword, phoneNumber})
+  });
+
+  const tokenResponseString = await tokenResponse.text();
+}
 
 const Login = () => {
   const [phoneNumber, setPhoneNumber] = useState("");
@@ -21,6 +41,12 @@ const Login = () => {
         keyboardType="numeric"
         secureTextEntry={true}
       />
+      <TouchableOpacity>
+        style={styles.button}
+        onPress={()=>{
+          sendText(phoneNumber);
+        }}
+      </TouchableOpacity>
     </SafeAreaView>
   );
 };
